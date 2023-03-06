107f6ddda742b42a926d3ec0bdbfce54d62743fc
Darya Clark <darya.clark@tufts.edu>
2023-03-06 18:22:50 -0500

    Add files via upload

--- motorPubPractice.py
+++ motorPubPractice.py
@@ -1,32 +1,16 @@
-# Title: Motor Publisher Practice
-# Author: Darya Clark
-# Purpose: Basic outline code to practice creating a publisher. This code
-#          continuously takes in user input and responds by moving the 
-#          create3 the specified angle and direction
-
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
 
-
 class PublisherNode(Node):
 
     def __init__(self):
+
         super().__init__('publisher_node')
         self.publisher_ = self.create_publisher(Twist, 'cmd_vel', 10)
 
-        # Set delay in seconds
-        timer_period = 1
-
-        # Creates a timer that triggers a callback function after the set timer_period
-        self.timer = self.create_timer(timer_period, self.publish_velocities)
-
-
-    # creates function for velocities 
     def publish_velocities(self):
-
         twist = Twist()
-
         # Set the linear velocity
         linVelocity = input('Enter a linear velocity (float): ') 
         twist.linear.x = float(linVelocity)
@@ -37,7 +21,16 @@ class PublisherNode(Node):
         twist.angular.z = float(angVelocity)
         print(twist.angular.z)
         
+        contVal = input('Continue? ')
+
+        if contVal == 'y':
+            cont = True
+        else: 
+            cont = False
+
         self.publisher_.publish(twist)
+
+        return cont
 
 
 def main(args=None):
@@ -45,17 +38,29 @@ def main(args=None):
     rclpy.init(args=args)
     
     node1 = PublisherNode()
-
     try:
-        # runs functions in publisher until keyboard interruption 
-        rclpy.spin(node1)
+        
+        loop = True
 
+        while loop:
+
+            cont = node1.publish_velocities()
+
+            if cont == False:
+
+                node1.destroy_node()
+                rclpy.shutdown()
+                
+                loop = False
+
+        # PublisherNode.destroy_node()
+        # rclpy.shutdown()
     except KeyboardInterrupt:
         print('\nCaught Keyboard Interrupt')
 
         # Destroys the node that was created
         node1.destroy_node()
-        # Resets 
+
         rclpy.shutdown()
 
 
